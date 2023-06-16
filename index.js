54677d34de3ddac649908d3fc2404a45eda32798
rohitnayal12 <rohitnayalkld@gmail.com>
2023-06-16 22:27:53 +0530

    cors issue solved

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080
 const connect = require('./config/db');
 const userRouter = require('./routes/user.route')
 app.use(express.json());
-
+app.use(cors())
 
 app.get('/api/', (req, res) => {
     return res.status(200).send({ massage: "hello vivek" })
